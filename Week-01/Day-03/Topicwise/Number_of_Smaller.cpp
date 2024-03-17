e160f2fb1e494bbbb45ab15c7070b8c154083f1c
Rishad Islam <rishad.islam158@gmail.com>
2024-03-17 10:46:28 +0600

    number of smaller

---
+++ Week-01/Day-03/Topicwise/Number_of_Smaller.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+    int arrA[n], arrB[m];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arrA[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> arrB[i];
+    }
+    int l = 0, r = 0;
+    int ans = 0;
+    while (r < m)
+    {
+        while (arrB[r] > arrA[l] && l < n)
+        {
+            ans++;
+            l++;
+        }
+       cout<<ans<<" ";
+        r++;
+    }
+}
