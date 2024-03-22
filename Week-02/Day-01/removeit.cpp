278267b9b17e4c427a6df0a7561f6dae3079c14d
rishadislam1 <rishad.islam158@gmail.com>
2024-03-22 16:31:15 +0600

    removeit

--- Week-02/Day-01/removeit.cpp
+++ Week-02/Day-01/removeit.cpp
@@ -1,6 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    v.erase(remove(v.begin(), v.end(), x), v.end());
+    for (auto it = v.begin(); it != v.end();
+         it++)
+        cout <<*it << ' ' ;
 }
